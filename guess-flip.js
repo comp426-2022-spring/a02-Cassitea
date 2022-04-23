79a8749e119f133351ea021091be5d90c7c35965
Cassitea <cassidynguyen3@gmail.com>
2022-04-23 01:01:30 -0400

    pls

---
+++ guess-flip.js
@@ -0,0 +1,7 @@
+import { flipACoin } from "./modules/coin.mjs";
+
+import {createRequire} from 'module';
+const args = require('minimist')(process.argv.slice(2))
+const require = createRequire(import.meta.url)
+
+console.log(flipACoin(args.call));
