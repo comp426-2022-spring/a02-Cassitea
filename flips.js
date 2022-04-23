31addca999d8d005dd9bc21784feff4da74c6f95
Cassitea <cassidynguyen3@gmail.com>
2022-04-23 02:01:36 -0400

    flips no longer workin

--- flips.js
+++ flips.js
@@ -1,7 +1,9 @@
 import {coinFlips, countFlips} from "./modules/coin.mjs";
+import minimist from "minimist";
 import {createRequire} from 'module';
 const require = createRequire(import.meta.url);
-const args = require('minimist')(process.argv.slice(2))
+
+const args = minimist(process.argv.slice(2))
 args["number"];
 var numFlips = coinFlips(args.number || 1) ;
 console.log(numFlips);
