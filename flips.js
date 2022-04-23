1c37a97d535f44001ab0ca446f07622ac016b1f8
Cassitea <cassidynguyen3@gmail.com>
2022-04-23 01:37:15 -0400

    typo in modules

--- flips.js
+++ flips.js
@@ -1,11 +1,9 @@
-import {coinFlips, countFlips} from './module/coin.mjs';
+import {coinFlips, countFlips} from "./modules/coin.mjs";
 import {createRequire} from 'module';
-
 const args = require('minimist')(process.argv.slice(2))
 const require = createRequire(import.meta.url);
-
 args["number"];
 var numFlips = coinFlips(args.number || 1) ;
-
 console.log(numFlips);
 console.log(countFlips(numFlips));
+/* y u no work */
